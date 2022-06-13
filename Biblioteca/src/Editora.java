5a2b78e1213353ce7a11098b070e771f72a9552f
Pedro Moraes <mrgpedrosilva@gmail.com>
2022-06-13 10:14:31 -0300

    Base da Classe Editora

---
+++ Biblioteca/src/Editora.java
@@ -0,0 +1,61 @@
+
+public class Editora {
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	
+	/**
+	 * Getters and Setters
+	 */
+	
+	/**
+	 * Busca Nome da editora
+	 * @return Nome da Editora
+	 */
+	public String getNome() {
+		return nome;
+	}
+	
+	/**
+	 * Atualiza nome da editora
+	 * @param nome Nome da editora
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * 
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	/**
+	 * 
+	 * @param telefone
+	 */
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
