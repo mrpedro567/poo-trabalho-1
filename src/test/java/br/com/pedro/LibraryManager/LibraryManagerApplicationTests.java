18fe13215b95a8ac7c42b30fdd4b62e8548ae30d
Pedro Moraes <pmoraes@asert.com.br>
2022-07-06 18:48:15 -0300

    Init SpringBoot

---
+++ src/test/java/br/com/pedro/LibraryManager/LibraryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.LibraryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
